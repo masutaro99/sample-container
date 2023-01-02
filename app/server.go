3a66216809713e635f584e025b07a8b2342221fc
Masukawa Kentaro <masukawa.kentaro@classmethod.jp>
2023-01-02 14:23:05 +0900

    add taro path

--- app/server.go
+++ app/server.go
@@ -13,5 +13,10 @@ func main() {
 			"message": "hello world!!",
 		})
 	})
+	engine.GET("/taro", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world!!taro",
+		})
+  })
 	engine.Run(":80")
 }
